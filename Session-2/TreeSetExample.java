bfc483a0f6c4d089c0289ee85520dcd55a4b6e30
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-11-30 10:40:31 +0530

    Core java Notes

---
+++ Session-2/TreeSetExample.java
@@ -0,0 +1,26 @@
+package com.simplilearn.collections;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetExample {
+	
+	public static void main(String[] args) {
+		//null values are not allowed
+		//it maintains insertion order
+		 Set<String> set= new TreeSet<String>();
+		 set.add("Apple");
+		 set.add("banana");
+		 set.add("Almond");
+		 //set.add(null);
+		 
+		 System.out.println(set);
+		 
+		 System.out.println("Size: "+set.size());
+		 
+		 System.out.println("Size: "+set.contains("Almond"));
+		 
+		 //itr using forLoop | iterator
+	}
+
+}
