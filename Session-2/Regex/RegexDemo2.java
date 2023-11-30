ef17234c1640027af7286caf4b056169ba7dc0ab
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-11-30 14:09:40 +0530

    Core java Notes

---
+++ Session-2/Regex/RegexDemo2.java
@@ -0,0 +1,24 @@
+package com.simplilearn.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexDemo2 {
+	
+	public static void main(String[] args) {
+		String regex="[987]{3}[0-9]{7}";
+		//first three numbers must be 987 , rest 7 must between 0-9
+		
+		String input="8794527814";
+		
+		Pattern pattern= Pattern.compile(regex);
+		Matcher match=pattern.matcher(input);
+		
+		if(match.matches()) {
+			System.out.println("Pattern Matched");
+		}else {
+			System.out.println("Invalid Input");
+		}
+	}
+
+}
