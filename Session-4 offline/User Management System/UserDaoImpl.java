e9025441c4ddc1f05bd20aeeb183bea3f9113b23
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-12-06 14:32:04 +0530

    SQL Query  Notes,JDBC,Management system,CRUD

--- Session-4 offline/User Management System/UserDaoImpl.java
+++ Session-4 offline/User Management System/UserDaoImpl.java
@@ -1,5 +1,9 @@
 package com.simplilearn.management;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoImpl implements UserDAO{
@@ -7,7 +11,37 @@ public class UserDaoImpl implements UserDAO{
 	@Override
 	public int createUser(User User) {
 		// TODO Auto-generated method stub
-		return 0;
+		int result=0;
+		Connection conn=DBConfig.getConnection();
+		if(conn!=null) {
+			 
+			try {
+				String query="insert into student(name,email,username) value(?,?,?)";
+				PreparedStatement stmt=conn.prepareStatement(query);
+				stmt.setString(1,User.getName());
+				stmt.setString(2,User.getEmail());
+				stmt.setString(3,User.getUsername());
+				
+				result=stmt.executeUpdate();
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(e);
+			}
+			finally {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}else {
+			System.out.println("Error While Connecting");
+		}
+		
+		return result;
 	}
 
 	@Override
