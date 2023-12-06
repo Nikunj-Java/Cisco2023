bcde9403997654a18b93f63050f19fc71ee3f872
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-12-06 15:16:14 +0530

    SQL Query  Notes,JDBC,Management system,JDBC CRUD

--- Session-4 offline/User Management System/UserManagementSystem.java
+++ Session-4 offline/User Management System/UserManagementSystem.java
@@ -8,26 +8,61 @@ public class UserManagementSystem {
 		UserDAO dao= new UserDaoImpl();
 		
 		Scanner sc = new Scanner(System.in);
+		System.out.println("PRESS 1 for Create Record \nPRESS 2 for Update Record\nPRESS 3 for Delete Record\nPRESS 4 for Retirve Record\nPRESS 5 for Retrive Record by Id");
+		
+		int input=sc.nextInt();
+		
+		switch (input) {
+			case 1:
+			//create reacord
+				System.out.println("**************INSERT RECORD**************");
+				sc.nextLine();
+				System.out.println("enter your name");
+				String name=sc.nextLine();
+				
 		 
-		System.out.println("enter your name");
-		String name=sc.nextLine();
+				System.out.println("enter your email");
+				String email=sc.nextLine();
+				
+			 
+				System.out.println("enter your username");
+				String username=sc.nextLine();
+				
+				int res=dao.createUser(new User(name, email, username));
+				
+				if(res>0) {
+					System.out.println("User Inserted Successfully");
+				}
+				else {
+					System.out.println("User Not Inserted");
+				}
+				
+			break;
+			case 2:
+			//update a record
+				System.out.println("**************UPDATE RECORD**************");
+			break;
+			
+			case 3:
+			//delete a record
+				System.out.println("**************DELETE RECORD**************");
+			break;
+			
+			case 4:
+			//retirve a record
+				System.out.println("**************FETCH RECORD**************");
+				break;
+				
+			
+			case 5:
+			//retrive by id
+				System.out.println("**************FETCH RECORD BY ID**************");
+				break;
+	
 		
- 
-		System.out.println("enter your email");
-		String email=sc.nextLine();
-		
-	 
-		System.out.println("enter your username");
-		String username=sc.nextLine();
-		
-		int res=dao.createUser(new User(name, email, username));
-		
-		if(res>0) {
-			System.out.println("User Inserted Successfully");
 		}
-		else {
-			System.out.println("User Not Inserted");
-		}
+		
+		 
 		
 		
 	}
