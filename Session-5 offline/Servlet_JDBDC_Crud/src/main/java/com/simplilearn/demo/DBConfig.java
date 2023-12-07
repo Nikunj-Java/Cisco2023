760aa9959a06f8fd251c37163d4c7aea667998cb
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-12-07 14:17:05 +0530

    Servlet Architecture,JDBC CRUD;

---
+++ Session-5 offline/Servlet_JDBDC_Crud/src/main/java/com/simplilearn/demo/DBConfig.java
@@ -0,0 +1,36 @@
+package com.simplilearn.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConfig {
+	
+	static Connection getConnection() {
+		
+		String driver="com.mysql.cj.jdbc.Driver";
+		String url="jdbc:mysql://localhost:3306/cisco1";
+		String username="root";
+		String password="Nikunj@123";
+		
+		Connection conn=null;
+		
+		try {
+			Class.forName(driver);
+			conn= DriverManager.getConnection(url,username,password);
+			
+			if(conn!=null)
+				return conn;
+			else 
+				return null;
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return conn;
+		
+		
+	}
+
+}
