11d86dfaaa70ca614c2d4ae86d392aa9dc3663fd
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-12-07 10:46:57 +0530

    Servlet Architecture;

---
+++ Session-5 offline/Servlet_07_12_2023/src/main/java/com/simplilearn/demo/FirstServelet.java
@@ -0,0 +1,21 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FirstServelet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=resp.getWriter();
+		
+		out.println("Welcome to first Servlet");
+	}
+
+}
---
+++ Session-5 offline/Servlet_07_12_2023/src/main/java/com/simplilearn/demo/SecondServelet.java
@@ -0,0 +1,31 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/second")
+public class SecondServelet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=resp.getWriter();
+		
+		out.print("Welcome to Annotations");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	
+	
+
+}
