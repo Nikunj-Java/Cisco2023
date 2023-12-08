b47ce958b09a88f8986f07d0517b51abe36daca3
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-12-08 09:32:13 +0530

    Url Rewritting

---
+++ Session-6 offline/UrlRewritting/src/main/java/com/simplilearn/demo/DashboardServlet.java
@@ -0,0 +1,34 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=resp.getWriter();
+		
+		String token=req.getParameter("userid");
+		
+		out.println("Welcome....!! "+token);
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	
+	
+
+}
---
+++ Session-6 offline/UrlRewritting/src/main/java/com/simplilearn/demo/LoginServlet.java
@@ -0,0 +1,33 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=resp.getWriter();
+		
+		String uname=req.getParameter("uname");
+		String url="dashboard?userid="+uname;
+		//out.println(uname);
+		resp.sendRedirect(url);
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+
+}
