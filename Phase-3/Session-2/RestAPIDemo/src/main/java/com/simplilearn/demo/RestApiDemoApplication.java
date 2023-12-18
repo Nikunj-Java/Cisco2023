197a2971de2c75129f162015fb9d08a1412a8103
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-12-18 15:12:23 +0530

    Spring JDBC Template,SpringBoot,Exception Handling,File Handling,Restful Api

---
+++ Phase-3/Session-2/RestAPIDemo/src/main/java/com/simplilearn/demo/RestApiDemoApplication.java
@@ -0,0 +1,16 @@
+package com.simplilearn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@ComponentScan("com.simplilearn.demo")
+public class RestApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDemoApplication.class, args);
+	}
+
+}
